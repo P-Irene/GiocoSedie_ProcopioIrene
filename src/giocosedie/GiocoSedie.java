55743949f5e2bc44aa3085b6d081b0e8886f5e7d
P-Irene <procopioirene1@gmail.com>
2025-01-15 20:01:07 +0100

    Aggiunta dell'input con classe Scanner

--- src/giocosedie/GiocoSedie.java
+++ src/giocosedie/GiocoSedie.java
@@ -5,33 +5,36 @@
 package giocosedie;
 
 import java.util.logging.Logger;
+import java.util.Scanner;
 /**
  *
  * @author Irene Procopio
  */
 public class GiocoSedie {
-    private final static int NUMSEDIE = 15;
     private static Logger logger = Logger.getLogger("GiocoSedie.GiocoSedie");
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Posto sedie[] = new Posto[NUMSEDIE];
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Scegliere il numero di partecipanti: ");
+        int num = myObj.nextInt();
+        Posto sedie[] = new Posto[num];
 
 	for (int k = 0; k < sedie.length; k++)
-		sedie[k] = new Posto();
+            sedie[k] = new Posto();
 
 	Display display = new Display(sedie);
 	//System.out.println("Sto facendo partire il Display.");
         logger.info("Sto facendo partire il Display.\n");
 	display.start();
  
-	Partecipante array[] = new Partecipante[NUMSEDIE+1];
-	for (int i = 0; i < NUMSEDIE + 1; i++) {
-		array[i] = new Partecipante(sedie);
-                //System.out.println("Sto facendo partire il thread n." + array[i].getId());
-                logger.info("Sto facendo partire il thread id: " + array[i].getId() + " name: " + array[i].getName() + "\n");
-                array[i].start();
+	Partecipante array[] = new Partecipante[num+1];
+	for (int i = 0; i < num + 1; i++) {
+            array[i] = new Partecipante(sedie);
+            //System.out.println("Sto facendo partire il thread n." + array[i].getId());
+            logger.info("Sto facendo partire il thread id: " + array[i].getId() + " name: " + array[i].getName() + "\n");
+            array[i].start();
         }
     }
     
