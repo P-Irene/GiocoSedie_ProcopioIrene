fd629650f787b4f1b48c90a95c634e70a2c1247a
P-Irene <procopioirene1@gmail.com>
2025-01-17 11:27:42 +0100

    Aggiunta della scrittura dei risultati nel file Risultato.txt

--- src/giocosedie/Partecipante.java
+++ src/giocosedie/Partecipante.java
@@ -4,12 +4,23 @@
  */
 package giocosedie;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Irene Procopio
  */
 public class Partecipante extends Thread {
     Posto sedie[];
+    String nomeFile = "Risultato.txt";
+    BufferedWriter br=null;
+    LocalDate localDate = LocalDate.now(ZoneId.of("GMT+1"));
     
     /**
      * Metodo Costruttore: inizializzazione dell'array sedie[]
@@ -17,7 +28,11 @@ public class Partecipante extends Thread {
      */
     public Partecipante(Posto sedie[]) {
 	this.sedie = sedie;
-
+        try {
+            br = new BufferedWriter(new FileWriter(nomeFile, true));
+        } catch (IOException ex) {
+            Logger.getLogger(Partecipante.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     public void run() {
@@ -26,12 +41,48 @@ public class Partecipante extends Thread {
             for (int i = 0; i < sedie.length; i++) {
 		if (sedie[i].occupa()) {
                     System.out.println("Sono il Thread " + this.getName() + ". Sono riuscito a sedermi sul posto " + i);
+                    scrivi(i);
                     return;
 		}
             }
             System.out.println("Sono il Thread " + this.getName() + ". Ho perso :((((");
-	} catch (InterruptedException e) {
+	}catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-  }
+    }
+    
+    public void scrivi(int i){
+        try {
+            if(i == 0){
+                br.write("Gioco Sedie " + this.localDate);
+                br.write("\n\r");
+                br.write("Posto n° " + i + " - ID Partecipante: " + this.getId());
+                br.write("\n\r");
+            }
+            else if(i == sedie.length - 1){
+                br.write("Posto n° " + i + " - ID Partecipante: " + this.getId());
+                br.write("\n\r");
+                br.write("-----------------------------------");
+                br.write("\n\r");
+            }
+            else{
+                br.write("Posto n° " + i + " - ID Partecipante: " + this.getId());
+                br.write("\n\r"); 
+            }
+            //3) svuoto il buffer e salvo nel file i dati
+            br.flush();         
+        }catch (IOException ex) {
+            Logger.getLogger(Partecipante.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        finally{
+            if (br!=null)
+                try {
+                    //4)chiudo lo stream in uscita
+                    br.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Partecipante.class.getName()).log(Level.SEVERE, null, ex);
+            }
+                
+        }
+    }
 }
